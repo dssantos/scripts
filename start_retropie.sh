eae837add720c4d62b322e306eb3467e69cf9f12
Danilo Santos <danilosantos1986@gmail.com>
2020-01-19 00:49:47 -0300

    Exits terminal when quit the retropie

--- start_retropie.sh
+++ start_retropie.sh
@@ -2,7 +2,7 @@
 # Run retropie emulator over raspbian desktop
 xdotool key "ctrl+alt+t"
 xdotool sleep 1
-xdotool type "ssh pi@localhost"
+xdotool type "ssh pi@localhost; exit"
 xdotool key KP_Enter
-xdotool type emulationstation
+xdotool type "emulationstation; exit"
 xdotool key KP_Enter
